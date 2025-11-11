0f77891c2bdf6b78d403a8432fbd4ded2bd74a15
Giuseppe Tornello <giuseppe.tornello05@protonmail.com>
2025-11-11 18:58:40 +0100

    refactor: moved match to a function

--- src/ex3.py
+++ src/ex3.py
@@ -41,18 +41,21 @@
 # }
 text_input = string(input("Enter a famous name+surname, ex. BarackObama: "))
 
-match text_input:
-    case "BarackObama":
-        print("44th president of the United States")
-    case "SandroPertini":
-        print("Former President of the Italian Republic")
-    case "NelsonMandela":
-        print("Former President of South Africa")
-    case "MahatmaGandhi":
-        print("Bapu")
-    case "DonaldKnuth":
-        print("Creator of LaTeX")
-    case "DennisRitchie":
-        print("Creator of C")
-    case _:
-        print("Invalid input! Please enter a good name!")
+print_output(text_input)
+
+def print_output(text_input) -> null:
+    match text_input:
+        case "BarackObama":
+            print("44th president of the United States")
+        case "SandroPertini":
+            print("Former President of the Italian Republic")
+        case "NelsonMandela":
+            print("Former President of South Africa")
+        case "MahatmaGandhi":
+            print("Bapu")
+        case "DonaldKnuth":
+            print("Creator of LaTeX")
+        case "DennisRitchie":
+            print("Creator of C")
+        case _:
+            print("Invalid input! Please enter a good name!")
