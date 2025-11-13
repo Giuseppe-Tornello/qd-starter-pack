aded8ab29ea8f8d101652a8de2a6726cfb3b5206
Giuseppe Tornello <giuseppe.tornello05@protonmail.com>
2025-11-13 15:25:39 +0100

    fix(sum.py): exercise was already made

--- src/sum.py
+++ src/sum.py
@@ -1,2 +1,8 @@
-def sum(a: int, b: int) -> int:
-    return a + b
+#
+#  Write a program that takes as input two numbers and print the sum.
+#
+#  Output:
+#  Insert the first number: 1
+#  Insert the second number: 2
+#  Sum: 3
+#
