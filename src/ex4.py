2d15799afd36e43adb7482ba74852e110cc0770f
Giuseppe Tornello <giuseppe.tornello05@protonmail.com>
2025-11-13 15:14:42 +0100

    refactor: added comment, improved readability

--- src/ex4.py
+++ src/ex4.py
@@ -55,15 +55,16 @@
 #   return 0;
 # }
 def print_month_days(month_number: int) -> None:
-    months_31 = [1,3,5,7,8,10,12]
-    months_30 = [11,4,6,9]
+    """Given a month (int 1-12) this function prints the number of days"""
+    months_31_list = [1,3,5,7,8,10,12]
+    months_30_list = [11,4,6,9]
     months_28 = 2
 
-    if month in months_31:
+    if month_number in months_31_list:
         print("31 days")
-    elif month in months_30:
+    elif month_number in months_30_list:
         print("30 days")
-    elif month == months_28:
+    elif month_number == months_28:
         print("28/29 days")
     else:
         print("Invalid input! Please enter month number between 1-12")
